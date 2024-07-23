4ac06d41a27b13de6665aa7e55820cf7ccb9a2c0
xonx4l <sk797119@gmail.com>
2024-07-23 23:03:48 +0530

    add fn in exp

--- src-tauri/src/filesystem/explorer.rs
+++ src-tauri/src/filesystem/explorer.rs
@@ -1,1 +1,11 @@
-// exp
+pub fn search() {
+    // search logic here 
+}
+
+pub fn item_found() {
+    // item_found logic here 
+}
+
+pub fn item_not_found() {
+    // item_not_found logic here 
+}
